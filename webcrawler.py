6b9264b0aa6f7b9fd483f4fe4609df068684de28
Jed Gore <jedgore@Jeds-Mac-mini.local>
2023-03-04 08:22:48 -0500

    update

--- webcrawler.py
+++ webcrawler.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd
 
-
+# class to save typing
 class WebCrawler:
     def __init__(self, base_url):
         self.base_url = base_url
@@ -18,15 +19,26 @@ class WebCrawler:
 
         return
 
-    def get_links(self):
+    def get_links(self, print_list=False):
         self.links = []
         for link in self.soup.find_all("a", href=True):
-            print(link["href"])
+            if print_list == True:
+                print(link["href"])
             self.links.append(link)
+        return
+
+    def get_tables(self, table_num=""):
+        self.tables = pd.read_html(self.base_url)
+        if table_num != "":
+            return self.tables[table_num]
+        return
 
 
 if __name__ == "__main__":
-    url = "https://practice.geeksforgeeks.org/courses/"
+    #    url = "https://practice.geeksforgeeks.org/courses/"
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+
     w = WebCrawler(url)
-    w.get_html_document()
-    w.get_links()
+    #    w.get_html_document()
+    #    w.get_links(print_list=True)
+    print(w.get_tables(1))
